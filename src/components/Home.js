67286764a74b7fd6f5a18407e569812c56bc4bd9
Rahul Golani <rahulgolani24@live.com>
2022-06-25 23:27:46 +0530

    Added Date Picker (react-date-rage)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../styles/Home.css";
 import Banner from "./Banner";
+import Search from "./Search";
 function Home() {
   return (
     <div className="home">
+      <Search />
       <Banner />
     </div>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,48 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+import "../styles/Search.css";
+function Search() {
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+      <Button
+        fullWidth
+        variant="outlined"
+        className="search__button"
+        onClick={() => setShowDatePicker(!showDatePicker)}
+      >
+        {showDatePicker ? "Hide" : "Search Dates"}
+      </Button>
+      {showDatePicker && (
+        <div className="search__datepicker">
+          <DateRangePicker
+            className="datepicker"
+            ranges={[selectionRange]}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Search;
