4fe4cfcb59e1450370fbfe4cc73197d5122e9f47
Rahul Golani <rahulgolani24@live.com>
2022-07-02 21:50:04 +0530

    Fixed Footer Content

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      {/* HEADER */}
       <Header />
-      {/* HOMEPAGE */}
-      <Home />
-      {/* SEARCH */}
-      {/* BANNER */}
-
-      {/* CARDS */}
-
+      <Routes>
+        <Route path="search" element={<SearchPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       {/* FOOTER */}
       <Footer />
-
       {/* SEARCHPAGE */}
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import CopyrightIcon from "@mui/icons-material/Copyright";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
 import "../styles/Footer.css";
 function Footer() {
   return (
     <div className="footer">
       <Typography className="footer__content" variant="body2">
         <CopyrightIcon fontSize="small" />
-        {` @${new Date().getFullYear()} - Airbnb -No Rights Reserved - This is a Demo `}
-        <FavoriteIcon fontSize="small" />
+        {` @${new Date().getFullYear()} - Airbnb -No Rights Reserved`}
       </Typography>
     </div>
   );
